131931d96d01c3b011941f1770f8394aa9e769d8
Jabir Mohammad <mjack1327@gmail.com>
2021-11-19 01:00:49 +0530

    added registration methods

--- smartcontracts/contracts/HealthChain.sol
+++ smartcontracts/contracts/HealthChain.sol
@@ -13,22 +13,98 @@ contract HealthChain {
 
     struct Patient {
         address payable patientId;
-        string name;
+        string fullName;
         uint256 age;
         string sex;
         uint256 createdDate;
-        address payable[] allowedDoctors;
+        address[] allowedDoctors;
         Prescription[] prescriptionNotes;
     }
 
     struct Doctor {
         address payable doctorId;
-        string name;
+        string fullName;
         string hospitalName;
+        string specialization;
+        uint256 createdDate;
     }
 
-    Patient[] private patientsList;
-    Doctor[] private doctorList;
+    mapping(address => Patient) private patientDataOf;
+    mapping(address => Doctor) public doctorDataOf;
+
+    event PatientRegistered(string fullName, uint256 age, string sex);
+    event DoctorRegistered(
+        string fullName,
+        string hospitalName,
+        string specialization
+    );
 
     constructor() {}
+
+    function registerPatient(
+        string memory _fullName,
+        uint256 _age,
+        string memory _sex
+    ) public returns (bool success) {
+        require(
+            bytes(patientDataOf[msg.sender].fullName).length <= 0,
+            "Registration already exists"
+        );
+        require(bytes(_fullName).length > 0, "Name should not empty");
+        require(_age > 0, "Age should not empty");
+        require(bytes(_sex).length > 0, "Sex should not empty");
+
+        Patient storage newPatient = patientDataOf[msg.sender];
+        newPatient.fullName = _fullName;
+        newPatient.age = _age;
+        newPatient.sex = _sex;
+        newPatient.createdDate = block.timestamp;
+
+        emit PatientRegistered(_fullName, _age, _sex);
+        return true;
+    }
+
+    function getPatientData(address _patientId)
+        public
+        view
+        returns (
+            string memory fullName,
+            uint256 age,
+            string memory sex
+        )
+    {
+        Patient memory patientData = patientDataOf[_patientId];
+
+        return (patientData.fullName, patientData.age, patientData.sex);
+    }
+
+    function registerDoctor(
+        string memory _fullName,
+        string memory _hospitalName,
+        string memory _specialization
+    ) public returns (bool success) {
+        require(
+            bytes(doctorDataOf[msg.sender].fullName).length <= 0,
+            "Registration already exists"
+        );
+        require(bytes(_fullName).length > 0, "Name should not empty");
+        require(
+            bytes(_hospitalName).length > 0,
+            "Hospital Name should not empty"
+        );
+        require(
+            bytes(_specialization).length > 0,
+            "Specialization should not empty"
+        );
+
+        Doctor storage newDoctor = doctorDataOf[msg.sender];
+        newDoctor.fullName = _fullName;
+        newDoctor.hospitalName = _hospitalName;
+        newDoctor.specialization = _specialization;
+        newDoctor.createdDate = block.timestamp;
+
+        emit DoctorRegistered(_fullName, _hospitalName, _specialization);
+
+        return true;
+    }
 }
--- smartcontracts/test/HealthChain.test.js
+++ smartcontracts/test/HealthChain.test.js
@@ -1,6 +1,13 @@
 const HealthChain = artifacts.require('./HealthChain.sol');
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bn')(BN))
+  .should();
 
-contract('HealthChain', ([deployer, uploader]) => {
+contract('HealthChain', (accounts) => {
+  const [deployer, uploader] = accounts;
+
   let healthChain;
 
   before(async () => {
@@ -21,6 +28,183 @@ contract('HealthChain', ([deployer, uploader]) => {
       const version = await healthChain.version();
       assert.equal(name, 'HealthChain');
       assert.equal(version, '0.1');
+    });
+  });
+
+  describe('Registration', () => {
+    const patientTestData = {
+      fullName: 'Jack Cooper',
+      age: new BN(24),
+      sex: 'male',
+    };
+
+    it('Patient registration', async () => {
+      await expectRevert(
+        healthChain.registerPatient(
+          '',
+          patientTestData.age,
+          patientTestData.sex,
+          {
+            from: uploader,
+          }
+        ),
+        'Name should not empty'
+      );
+      await expectRevert(
+        healthChain.registerPatient(
+          patientTestData.fullName,
+          0,
+          patientTestData.sex,
+          {
+            from: uploader,
+          }
+        ),
+        'Age should not empty'
+      );
+      await expectRevert(
+        healthChain.registerPatient(
+          patientTestData.fullName,
+          patientTestData.age,
+          '',
+          {
+            from: uploader,
+          }
+        ),
+        'Sex should not empty'
+      );
+
+      const success = await healthChain.registerPatient.call(
+        patientTestData.fullName,
+        patientTestData.age,
+        patientTestData.sex,
+        {
+          from: uploader,
+        }
+      );
+      assert.equal(success, true, 'It returns true');
+
+      const receipt = await healthChain.registerPatient(
+        patientTestData.fullName,
+        patientTestData.age,
+        patientTestData.sex,
+        {
+          from: uploader,
+        }
+      );
+      expectEvent(receipt, 'PatientRegistered', {
+        fullName: patientTestData.fullName,
+        age: patientTestData.age,
+        sex: patientTestData.sex,
+      });
+
+      const newlyRegsiteredPatient = await healthChain.getPatientData(uploader);
+      assert.equal(newlyRegsiteredPatient.fullName, patientTestData.fullName);
+      assert.equal(newlyRegsiteredPatient.age.toNumber(), patientTestData.age);
+      assert.equal(newlyRegsiteredPatient.sex, patientTestData.sex);
+
+      await expectRevert(
+        healthChain.registerPatient(
+          patientTestData.fullName,
+          patientTestData.age,
+          patientTestData.sex,
+          {
+            from: uploader,
+          }
+        ),
+        'Registration already exists'
+      );
+    });
+
+    const doctorTestData = {
+      fullName: 'Jack Cooper',
+      hospitalName: 'Starcare',
+      specialization: 'Cardiologist',
+      address: accounts[0],
+    };
+
+    it('Doctor registration', async () => {
+      await expectRevert(
+        healthChain.registerDoctor(
+          '',
+          doctorTestData.hospitalName,
+          doctorTestData.specialization,
+          {
+            from: doctorTestData.address,
+          }
+        ),
+        'Name should not empty'
+      );
+      await expectRevert(
+        healthChain.registerDoctor(
+          doctorTestData.fullName,
+          '',
+          doctorTestData.specialization,
+          {
+            from: doctorTestData.address,
+          }
+        ),
+        'Hospital Name should not empty'
+      );
+      await expectRevert(
+        healthChain.registerDoctor(
+          doctorTestData.fullName,
+          doctorTestData.hospitalName,
+          '',
+          {
+            from: doctorTestData.address,
+          }
+        ),
+        'Specialization should not empty'
+      );
+
+      const success = await healthChain.registerDoctor.call(
+        doctorTestData.fullName,
+        doctorTestData.hospitalName,
+        doctorTestData.specialization,
+        {
+          from: doctorTestData.address,
+        }
+      );
+      assert.equal(success, true, 'It returns true');
+
+      const receipt = await healthChain.registerDoctor(
+        doctorTestData.fullName,
+        doctorTestData.hospitalName,
+        doctorTestData.specialization,
+        {
+          from: doctorTestData.address,
+        }
+      );
+      expectEvent(receipt, 'DoctorRegistered', {
+        fullName: doctorTestData.fullName,
+        hospitalName: doctorTestData.hospitalName,
+        specialization: doctorTestData.specialization,
+      });
+
+      const newlyRegsiteredDoctor = await healthChain.doctorDataOf(
+        doctorTestData.address
+      );
+      assert.equal(newlyRegsiteredDoctor.fullName, doctorTestData.fullName);
+      assert.equal(
+        newlyRegsiteredDoctor.hospitalName,
+        doctorTestData.hospitalName
+      );
+      assert.equal(
+        newlyRegsiteredDoctor.specialization,
+        doctorTestData.specialization
+      );
+
+      await expectRevert(
+        healthChain.registerDoctor(
+          doctorTestData.fullName,
+          doctorTestData.hospitalName,
+          doctorTestData.specialization,
+          {
+            from: doctorTestData.address,
+          }
+        ),
+        'Registration already exists'
+      );
     });
   });
 });
