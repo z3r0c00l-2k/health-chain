ec0c73d13d5a5140c8abb87573bfa32eab6aec19
Jabir Mohammad <mjack1327@gmail.com>
2021-11-24 00:00:22 +0530

    contracts deployed to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,23 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const privateKeys = process.env.PRIVATE_KEYS || '';
+
 module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*',
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          privateKeys.split(','), // Array of account private keys
+          `wss://ropsten.infura.io/ws/v3/${process.env.INFURA_PROJECT_ID}` // Url to an Ethereum Node
+        ),
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 3,
     },
   },
   mocha: {},
